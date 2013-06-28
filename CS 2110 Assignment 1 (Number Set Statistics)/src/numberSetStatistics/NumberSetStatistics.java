138ad918c3bdf38d3ad7b310738c9510424abce7
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-06-27 22:45:11 -0400

    file will output relevant info

--- CS 2110 Assignment 1 (Number Set Statistics)/src/numberSetStatistics/NumberSetStatistics.java
+++ CS 2110 Assignment 1 (Number Set Statistics)/src/numberSetStatistics/NumberSetStatistics.java
@@ -1,6 +1,8 @@
 package numberSetStatistics;
 
 //Import Classes Necessary for Program
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.FileReader;
@@ -36,21 +38,22 @@ public class NumberSetStatistics {
 			double[] numberSet;
 			
 			messageOut.println("Give the path of a file or type in your data.");
-			String path=userInput.readLine();
+			String inputFilePath=userInput.readLine();
 			String input = "";
-			File file = new File(path);
+			File file = new File(inputFilePath);
 			if (file.exists() && file.canRead()) {
-				FileReader fr = new FileReader(path);
+				FileReader fr = new FileReader(inputFilePath);
 				userInput = new BufferedReader(fr);
 				String line =userInput.readLine();
 				while(line !=null){
 					input+=line + " ";
 					line=userInput.readLine();					
 				}
+				userInput = new BufferedReader(isr);
 			} else { 
 				input=userInput.readLine();
 				}
-
+			String save = ""; 
 			String[] numberSets = input.split("[^0-9\\.\\s]+");
 			for (int i = 0;i<numberSets.length;i++) {
 				
@@ -59,6 +62,7 @@ public class NumberSetStatistics {
 				
 				if (aSet.isEmpty()) {
 					messageOut.println("I just read an empty set so there no statistics.");
+					save += "I just read an empty set so there no statistics.\n";
 					System.exit(0);
 				}//Close if
 				
@@ -76,6 +80,7 @@ public class NumberSetStatistics {
 				
 				if (numberSetString.length==1){
 					//print out message
+					min = max = avg =Double.parseDouble(numberSetString[0]);
 					messageOut.println("avg,min,and max are all the same thing. Stdev does not exist.");
 					if ((i+1)<numberSets.length) {
 						i++;
@@ -122,8 +127,10 @@ public class NumberSetStatistics {
 				variance = stdev*stdev;
 
 				messageOut.print("\nFor set [");
+				save += "\nFor set [";
 				for (int m=0; (m)<numberSet.length;m++) {
 					messageOut.print(numberSet[m]);
+					save += numberSet[m];
 					if ((m+1)<numberSet.length) {
 						messageOut.print(",");
 					}
@@ -132,24 +139,36 @@ public class NumberSetStatistics {
 				messageOut.println("\tThe minimum of the set is " + min);
 				messageOut.println("\tThe maximum of the set is " + max);
 				messageOut.println("\tThe average of the set is " + avg);
-				messageOut.println("\tThe standard deveation is " + stdev);
+				messageOut.println("\tThe standard deviation is " + stdev);
 				messageOut.println("\tThe variance is " + variance);
 				messageOut.println("\tThe Sum of the residuals is " + residualSum);
+				save += "]\n\tThe minimum of the set is " + min + "\n\tThe maximum of the set is " + max
+						+ "\n\tThe average of the set is " + avg + "\n\tThe standard deviation is " + stdev
+						+ "\n\tThe variance is " + variance + "\n\tThe Sum of the residuals is " + residualSum +"\n";
 				}//Close else
 				messageOut.println("\nNew Set of Numbers");
 				
 				}//Close for
-				//Close If
-			//else if (input=="file") {useFile();}
-			//else {messageOut.println("Please tell me Either\"type\" or \"file\" or \"close\".");}
+			messageOut.println("Do you want to save these stats? (\"yes\" or \"no\")");
+			String response=userInput.readLine();
+			if(response.equals("no")) {
+				System.out.println("OK. Thank you for using this program for calculating statistics");
+			
+			} else if (response.equals("yes")){
+					messageOut.println("Type name of file you wish to send stats to");
+					String outputFilePath=userInput.readLine();
+					file= new File(outputFilePath);
+					file.createNewFile();
+					FileWriter writer=new FileWriter(file.getAbsoluteFile());
+					BufferedWriter buffered= new BufferedWriter(writer);
+					buffered.write(save);
+					buffered.close();
+					System.out.println("Okay, you can find it at " + outputFilePath);
+			} else {
+				messageOut.println("Sorry i didn't understand you, but thank you" +
+						" for using this program to calculate statistics");
+			}
 
 	}//Close main Method
-
-
-	
-	//Create a saving method
-	public static void saveFile() {
-	
-	}//Close saveFile Method
 }//Close Class NumberSetStatistics 
 
